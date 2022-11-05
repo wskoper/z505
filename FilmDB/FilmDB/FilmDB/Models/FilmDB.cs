20e287c31e0f6fe6de2716c638a1edd08884e060
Wojciech Koper <w_koper@ms.wwsi.edu.pl>
2022-11-05 16:34:12 +0100

    init repo

---
+++ FilmDB/FilmDB/FilmDB/Models/FilmDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB.Models
+{
+    public class FilmDB
+    {
+        public int ID { get; set; }
+        //[ID] [int] IDENTITY(1,1) NOT NULL;
+        public string Title { get; set; }
+        //[TITLE] [VNCHAR(200)] NOT NULL;
+        public int Year { get; set; }
+    }
+}
--- FilmDB/FilmDB/FilmDB/Startup.cs
+++ FilmDB/FilmDB/FilmDB/Startup.cs
@@ -50,7 +50,7 @@ namespace FilmDB
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Film}/{action=Index}/{id?}");
             });
         }
     }
