09ea186deb33c86efd5d6bf2c88b4a9fe1feb230
Wojciech Koper <w_koper@ms.wwsi.edu.pl>
2022-12-03 16:59:48 +0100

    zjazd5

--- FilmDB/FilmDB/FilmDB/Controllers/FilmController.cs
+++ FilmDB/FilmDB/FilmDB/Controllers/FilmController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FilmDB.Logic;
+using FilmDB.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,26 @@ namespace FilmDB.Controllers
     public class FilmController : Controller
     {
         public IActionResult Index()
+        {
+            FilmManager filmManager = new FilmManager();
+            FilmModel film = new FilmModel()
+            {
+                ID = 1,
+                Title = "Rambo",
+                Year = 1980
+            };
+            filmManager.AddFilm(film);
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(FilmModel filmModel)
+        {
+            FilmManager filmManager = new FilmManager();
+            filmManager.AddFilm(filmModel);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Add()
         {
             return View();
         }
---
+++ FilmDB/FilmDB/FilmDB/Logic/FilmManager.cs
@@ -0,0 +1,101 @@
+﻿using FilmDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FilmDB.Logic
+{
+    public class FilmManager
+    {
+        private FilmModel filmModel;
+
+        public FilmManager AddFilm(FilmModel filmModel)
+        {
+
+            using (var context = new FilmContext())
+            {
+                try
+                {
+                    context.Films.Add(filmModel);
+                    context.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    if (filmModel.ID !=0)
+                    {
+                        filmModel.ID = 0;
+                        context.Films.Add(filmModel);
+                        context.SaveChanges();
+                    }
+                }
+             }
+            return this;
+        }
+
+        public FilmManager RemoveFilm(int id)
+        {
+
+
+            using (var context = new FilmContext())
+            {
+
+                var film = context.Films.Single(x => x.ID == id);
+                     context.Films.Remove(film);
+                     context.SaveChanges();
+                        
+               
+            }
+            return this;
+        }
+
+        public FilmManager UpdateFilm(FilmModel filmModel)
+        {
+            using(var context = new FilmContext())
+            {
+                context.Films.Update(filmModel);
+                context.SaveChanges();
+            }
+            return this;
+        }
+
+        public FilmManager ChangeTitle(int id, string newTitle)
+        {
+            using (var context = new FilmContext())
+            {
+                var film = context.Films.Single(x => x.ID == id);
+                if (String.IsNullOrEmpty(newTitle))
+                {
+                    newTitle = "Brak tytułu";
+                }
+                film.Title = newTitle;
+                this.UpdateFilm(film);
+            }
+            return this;
+            
+        }
+
+        public FilmModel GetFilm(int id)
+        {
+            using (var context = new FilmContext())
+            {
+
+                var film = context.Films.Single(x => x.ID == id);
+                return film;
+
+            }
+           
+        }
+
+        public List<FilmModel> GetFilms()
+        {
+            using (var context = new FilmContext())
+            {
+
+                var film = context.Films.ToList();
+                return film;
+            }                   
+        }
+    }
+}
